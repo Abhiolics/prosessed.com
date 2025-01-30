f2c1df386f89f2aeef84e0f6bb23626054324227
Abhay Pratap Singh <138395064+Abhiolics@users.noreply.github.com>
2025-01-30 16:13:55 +0530

    OG Tags

--- app/page.tsx
+++ app/page.tsx
@@ -30,13 +30,23 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <>
-    <Head>
+     <Head>
+        {/* Open Graph Meta Tags */}
         <meta property="og:title" content="AI Powered Order Management Platform for Food Importers and Exporters" />
         <meta property="og:site_name" content="Prosessed" />
         <meta property="og:url" content="https://www.prosessed.ai/" />
         <meta property="og:description" content="Streamline your cross-border operations with AI-driven tools from quote to payment." />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.prosessed.ai/images/HeroImg.svg" />
+        <meta property="og:image" content="https://www.prosessed.ai/images/HeroImg.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="AI Powered Order Management Platform for Food Importers and Exporters" />
+        <meta name="twitter:description" content="Streamline your cross-border operations with AI-driven tools from quote to payment." />
+        <meta name="twitter:image" content="https://www.prosessed.ai/images/HeroImg.png" />
       </Head>
       <div className="relative max-w-6xl mx-auto h-0 pointer-events-none -z-1 " aria-hidden="true">
         <PageIllustration />
